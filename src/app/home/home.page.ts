90e5fcb25279f4facbc7ce12a6cd666714c956ce
LEXXAR <jiubermq97@gmail.com>
2020-01-09 15:58:51 -0600

    segunda parte

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,58 +1,140 @@
-import { Component } from '@angular/core';
-import { AlertController, } from '@ionic/angular';
+import {Component} from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor(private alertController: AlertController) {}
+    cardsData = [
+        {
+            title: 'Hola equipo',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 2',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        },
+        {
+            title: 'Hola equipo 3',
+            subtitle: 'El agua esta bien',
+            contentText: `Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate,
+            nam quidem. Adipisci aliquam eaque hic nulla
+            suscipit. Assumenda atque consequuntur esse eum
+             maxime mollitia obcaecati repellendus vel veritatis
+             voluptate. Fugit.`
+        }
+    ];
 
+    constructor(private alertController: AlertController) {
+    }
 
-  onBeerClicked(){
-    console.log("hola")
-  }
+    onBeerClicked() {
+        console.log('quiero una no tan fria!');
+    }
 
-  openAlert() {
-    this.alertController.create({
-        header: 'Hello world!',
-        subHeader: '¡Los baños estan mas limpios ahora!',
-        buttons: [
-            {
-                text: 'Cerrar',
-                handler: () => {
-                    console.log('bye bye!');
+    openAlert() {
+        this.alertController.create({
+            header: 'Hello world!',
+            subHeader: '¡Los baños estan mas limpios ahora!',
+            buttons: [
+                {
+                    text: 'Cerrar',
+                    handler: () => {
+                        console.log('bye bye!');
+                    }
                 }
-            }
-        ]
-    }).then(alert => {
-        return alert.present();
-    }).then(() => {
-        console.log('ya se terminó de abrir 100% seguro');
-    });
+            ]
+        }).then(alert => {
+            return alert.present();
+        }).then(() => {
+            console.log('ya se terminó de abrir 100% seguro');
+        });
 
-}
-  
-
-
-  cardsData = [{
-    title: 'Hola a todos',
-    subtitle: 'El agua esta bien',
-    contentText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis libero in, iure nam culpa unde cupiditate asperiores doloremque! Error, quis. Culpa, dolorem nam. Ratione ea esse dolores, non saepe iure?'
-  },
-  {
-    title: 'Hola a todos 2',
-    subtitle: 'El agua esta bien',
-    contentText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis libero in, iure nam culpa unde cupiditate asperiores doloremque! Error, quis. Culpa, dolorem nam. Ratione ea esse dolores, non saepe iure?'
-  },
-  {
-    title: 'Hola a todos 3',
-    subtitle: 'El agua esta bien',
-    contentText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis libero in, iure nam culpa unde cupiditate asperiores doloremque! Error, quis. Culpa, dolorem nam. Ratione ea esse dolores, non saepe iure?'
-  }
-
-];
-
+    }
 }
